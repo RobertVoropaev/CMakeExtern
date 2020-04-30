e204a043d3aa59a9ce57d68fa92372c1c06a9ce1
RobertVoropaev <robertvoropaev11@gmail.com>
2020-04-30 10:36:58 +0300

    rm build/*

--- build/CMakeFiles/generate_header.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/generate_header"
-  "../B/C/index.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate_header.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
