8c1a79fbf151d0459cc84d9bcef9016531f3303d
RobertVoropaev <robertvoropaev11@gmail.com>
2020-04-30 10:53:15 +0300

    add doxygen comment

--- B/A/lib.h
+++ B/A/lib.h
@@ -1,3 +1,13 @@
+/*!
+     \file
+     \brief Главный файл библиотеки
+*/
 #include <iostream>
+
+/*!
+     \brief Главная функция библиотеки библиотеки
+
+     Печатает в stdin "Hello, i'm lib file!"
+*/
 
 void print_hello_lib();
--- B/C/preparing.py
+++ B/C/preparing.py
@@ -23,7 +23,19 @@ def create(filename, content):
   open(filename, 'w').write(content)
 
 content = """
+/*!
+     \file
+     \brief Сгенерированный заголовочный файл
+*/
+
 #include <iostream>
+
+/*!
+     \brief Главная функция сгенерированного заголовочного файла
+
+     Печатает в stdin "Hello, i'm index file!"
+*/
+
 
 void print_hello_index(){
 	std::cout << "Hello, i'm index file!" << std::endl;
